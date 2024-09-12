e58ab4f5623662f8bae1dcdb7dd0ffc4735cf0c1
Mubeen-Anxari <mubeenansari10001@gmail.com>
2024-09-12 19:50:06 +0500

    add the domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['prada-ten.vercel.app'],  // Only the domain name, no protocol or path
+    },
+  };
+  
+  export default nextConfig;
+  
